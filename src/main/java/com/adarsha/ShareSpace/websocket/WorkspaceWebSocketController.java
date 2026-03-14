79399508731680074edcbb5a853687e1db4ab587
adarshagc <adarshagc2004@gmail.com>
2026-03-14 23:19:41 +0530

    Small changes in Websocket

--- src/main/java/com/adarsha/ShareSpace/websocket/WorkspaceWebSocketController.java
+++ src/main/java/com/adarsha/ShareSpace/websocket/WorkspaceWebSocketController.java
@@ -1,26 +1,30 @@
 package com.adarsha.sharespace.websocket;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
-
 import com.adarsha.sharespace.dto.TextMessage;
 import com.adarsha.sharespace.service.WorkspaceService;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
 
 @Controller
-@RequiredArgsConstructor
 public class WorkspaceWebSocketController {
-    
-    private final SimpMessagingTemplate messagingTemplate;
+
     private final WorkspaceService workspaceService;
 
-    @MessageMapping("/workspace.update")
-    public void updateText(TextMessage message) {
-        workspaceService.updateText(message.getCode(), message.getTextContent());
-        messagingTemplate.convertAndSend(
-            "/topic/workspace/" + message.getCode(),
-            message
-        );
-    } 
+    public WorkspaceWebSocketController(WorkspaceService workspaceService) {
+        this.workspaceService = workspaceService;
+    }
+
+    @MessageMapping("/workspace/{code}")
+    @SendTo("/topic/workspace/{code}")
+    public TextMessage updateText(
+            @DestinationVariable String code,
+            TextMessage message
+    ) {
+
+        workspaceService.updateText(code, message.getTextContent());
+
+        return message;
+    }
 }
